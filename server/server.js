ec98bd44270fef656126f6ac4c8f2b66faf3795b
SEONGMIN <sungmin3790@gmail.com>
2025-11-21 15:07:40 +0900

    25.11.21 sns 샘플 업로드

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors') 
+
+const userRouter = require("./routes/user")
+
+const app = express()
+app.use(cors({
+    origin : "*",
+    credentials : true
+}))
+app.use(express.json());
+
+// router 영역
+app.use("/user", userRouter);
+
+app.listen(3020, ()=>{
+    console.log("server start!");
+})
